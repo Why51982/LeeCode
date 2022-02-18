655028426fa7d25d8c6cc0a4300c1b52ed211063
Ocean <997571972@qq.com>
2022-02-19 02:52:14 +0800

    提交

---
+++ LeeCode/LeeCode/main.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  LeeCode
+//
+//  Created by Ocean on 2022/2/18.
+//
+
+import Foundation
+
+// 只出现一次数字
+//let nums = [4,1,2,1,2]
+//let num = SingleNum.singleNumber(nums)
+//print(num)
+
+// 多数元素
+//let nums = [6, 5, 5]
+//let num = MoreNum.majorityElement1(nums)
+//let num = MoreNum.majorityElement2(nums)
+//debugPrint(num)
+
+// 搜索二维矩阵
+//let matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
+//let isHas = MatrixNum.searchMatrix(matrix, 31)
+//debugPrint(isHas)
+
+// 合并两个有序数组
+var nums1 = [2, 0]
+let m = 1, nums2 = [1], n = 1
+MergeArray.merge(&nums1, m, nums2, n)
+debugPrint(nums1)
